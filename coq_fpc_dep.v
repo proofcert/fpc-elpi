2923f88ed0edad80d85d1be78812ea81d0296e4c
Matteo <mmanighe@lix.polytechnique.fr>
2020-02-19 10:41:00 +0100

    Cleanup examples. Unify term with tactic Ev after kernel run

--- coq_fpc_dep.v
+++ coq_fpc_dep.v
@@ -1,11 +1,13 @@
+(* Check ex.
+Check or_ind. *)
 (* An example file using the kernel with dependent types and direct term construction *)
 From elpi Require Import elpi.
 Elpi Db coq_fpc.db lp:{{
   solve [(int N)] [goal Ctx Ev Ty _] [] :- 
     int_to_nat N Nat,
-    Ctx => ljf_entry (dd Nat) Ty Ev, coq.say "Finished" . 
+    Ctx => ljf_entry (dd Nat) Ty Ev1, Ev = Ev1. %% It looks like the unification forces some sort of normalization
+                                                %% This is needed for the tactic to work
 }}.
-
 Elpi Tactic coq_fpc.
 Elpi Accumulate File "fpc/ljf-dep.mod".
 Elpi Accumulate File "fpc/dd-fpc.mod".
@@ -13,8 +15,18 @@ Elpi Accumulate Db coq_fpc.db.
 Elpi Typecheck.
  
 Elpi Debug "DEBUG".
-(* This query is problematic! The forall-left rule causes a problem with
-   bound variables in Coq *)
+
+Elpi Query lp:{{ljf_entry (dd (s (s (s zero)))) {{forall P : nat -> Prop, (exists x, P x)  -> (exists x, P x)}} Term.}}.
+Goal forall P : nat -> Prop, (exists x, P x)  -> (exists x, P x).
+elpi coq_fpc 2.
+Qed.
+Elpi Query lp:{{coq.typecheck {{(fun (P : nat -> Prop) (H : exists x : nat, P x) => H) }} Ty.}}.
+Elpi Query lp:{{ljf_entry (dd (s (s (s zero)))) {{forall P : nat -> Prop, (exists x, P x)  -> (exists x, P x)}} {{(fun (P : nat -> Prop) (H : exists x : nat, P x) => H)}}.}}.
+Goal forall P : nat -> Prop, (exists x, P x)  -> (exists x, P x).
+elpi coq_fpc 3.
+Goal forall P Q : Type -> Prop, forall x, forall y, ((P x) -> (Q y)) -> (exists x, (P x) -> forall y, (Q y)).
+elpi coq_fpc 2.
+Elpi Query lp:{{ljf_entry (dd (s (s zero))) {{forall (Q: nat -> Prop), (forall x : nat, Q x) -> (forall x:nat, Q x)}} J}}.
 Goal forall (Q: nat -> Prop), (forall x : nat, Q x) -> (forall x:nat, Q x).
 Proof.
   elpi coq_fpc 2.
@@ -23,6 +35,25 @@ Qed.
    It builds the expected term *)
 (* Elpi Query lp:{{ljf_entry (dd (s (s zero))) {{forall (Q: Type -> Prop), (forall x : Type, Q x) -> (forall x:Type, Q x)}} J}}.
 
+(* Elpi Trace. *)
+Elpi Query lp:{{ljf_entry (dd (s (s (s zero))))  {{forall (P: Prop) (Q: Type -> Prop), (forall x, P -> Q x) -> P -> (forall x, Q x)}}
+X}}.
+Elpi Query lp:{{ljf_entry (dd  (s (s zero))) {{forall P:Prop, (P->P)}} X}}.
+Elpi Query lp:{{ljf_entry (dd  (s (s zero))) {{forall P Q:Prop, (P->Q) -> P -> Q}} X}}.
+Goal forall P Q:Prop, (P->Q) -> P -> Q.
+elpi coq_fpc 2.
+Qed.
+
+*)
+Elpi Query lp:{{ljf_entry (dd (s zero)) {{forall P: Prop, P \/ P -> P}} X}}.
+
+Goal forall P : Prop, P -> P.
+elpi coq_fpc 1.
+Qed.
+
+(* Goal forall P Q : Prop, P \/ P -> P.
+elpi coq_fpc 1.
+Qed. *)
 Goal forall (Q: Prop), Q -> (forall x:nat, Q).
 Proof.
   elpi coq_fpc 2.
@@ -35,10 +66,12 @@ Qed.
 Goal forall (P: Prop) (Q: Type -> Prop), (forall x, P -> Q x) -> P -> (forall x, Q x).
 Proof.
   elpi coq_fpc 2.
-End.
-Elpi Debug "DEBUG".
-(* Elpi Trace. *)
-Elpi Query lp:{{ljf_entry (dd (s zero)) {{forall A: Prop, A -> A}} J.%{{fun (A:Prop) (X:A) => X}}}}.
+Qed.
 
-Lemma test : forall A: Prop, A -> A.
-elpi coq_fpc_ng 1.
+Goal forall A: Prop, A -> A.
+elpi coq_fpc 1.
+Qed.
+
+Goal forall A B: Prop, A \/ B -> B \/ A.
+elpi coq_fpc 1.
+Qed.
