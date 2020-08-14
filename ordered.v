0b0b52784eadb6a4e210b9887075d7e67a958831
Matteo <mmanighe@lix.polytechnique.fr>
2020-08-14 17:37:40 +0200

    Coq experiments

---
+++ ordered.v
@@ -0,0 +1,38 @@
+From elpi Require Import elpi.
+Require Import Arith List. Import ListNotations.
+Require Import Coq.Lists.List.
+
+Inductive ordered : list nat -> Prop :=
+	onl : ordered []
+  | oss : forall x : nat, ordered [x]
+  | ocn : forall (x y : nat) (l : list nat),
+         ordered (y :: l) -> x <= y -> ordered (x :: y :: l)
+.
+Elpi Command pbt.
+Elpi Accumulate File "pbt/src/kernel.mod".
+Elpi Typecheck.
+Fail Elpi Query lp:{{
+  interp {{ordered [1;0;2].}}.
+  }}.
+
+Elpi Query lp:{{
+  interp {{ordered [1;1;2].}}.
+  }}.
+
+Elpi Query lp:{{
+coq.locate "le" (indt LE), coq.env.indt LE _ _ _ T C Ts.
+}}.
+
+Elpi Query lp:{{
+  coq.locate "ocn" Name, coq.env.typeof Name Ty, gethead Ty Hd Args.
+}}.
+
+Elpi Query lp:{{
+  coq.locate "ordered" GR,   coq.say "ordered is:" GR,
+  coq.env.typeof GR Ty, coq.say "The type of ordered is:" Ty.  
+}}.
+
+Elpi Query lp:{{
+    coq.locate "ordered" (indt GR),
+    coq.env.indt GR _tt _uhm _mah Type Kn Types.   % get the names of the constructors
+ }}.
