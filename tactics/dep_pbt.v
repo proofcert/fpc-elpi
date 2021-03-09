7e168f05171c60ae2bbae0805a057abef6d5c793
Matteo Manighetti <mmanighe@lix.polytechnique.fr>
2021-03-09 17:45:15 +0100

    More comments and parametrized cert code

--- tactics/dep_pbt.v
+++ tactics/dep_pbt.v
@@ -39,6 +39,7 @@ Elpi Accumulate lp:{{
   build_clauses [decl Var _Name _Ty] [(copy Var X_ :- !)].
   build_clauses [decl Var _Name _Ty | Ds] [(copy Var X_ :- !)| Cs] :-
     build_clauses Ds Cs.
+
   %% env_clauses: given a Coq context creates copy clauses associating
   %% an eigenvariable to its type. 
   % As before, the goal context is given as a list of triplets (eigenvariable,
@@ -46,8 +47,6 @@ Elpi Accumulate lp:{{
   % copies each eigenvariable to its type. Indeed, the tactic will be invoked
   % with a list of parameters that are simply eigenvariables: c1, c3, ... With
   % this predicate, we wish to create goals of the intended types.
-
-
   type env_clauses goal-ctx -> list prop -> prop.
   env_clauses [decl Var _ Ty] [(copy Var Ty :- !)].
   env_clauses [decl Var _ Ty |L] [(copy Var Ty :- !)|Cs] :-
@@ -57,17 +56,38 @@ Elpi Accumulate lp:{{
   type remove_trm list argument -> list term -> prop.
   remove_trm [trm A] [A].
   remove_trm [trm A | R] [A|R'] :- remove_trm R R'.
+
+  %% preprocess: combines the previous predicates.given a context and a list
+  %% of arguments (intended to contain specification variables), returns
+  %% three lists
+  %% - The specification eigenvariables extracted from the argument
+  %% - A list of copy clauses associating such eigenvariables to fresh logic variables
+  %% - A list of copy clauses associating the eigenvariables to their type in the context
+  %% Thus if the context has shape
+  %% c1 : A
+  %% c2 : (B c1)
+  %% c3 : C
+  %% And the supplied list of arguments is (c1) (c2), the three output list are
+  %% [c1, c2]
+  %% [copy c1 X0, copy c2 X1]
+  %% [copy c1 A, copy c2 (B c1)]
   %          +   +     -   -     -
-  % type preprocess goal-ctx -> list argument -> list term -> ? -> ? -> prop.
+  type preprocess goal-ctx -> list argument -> list term -> list prop -> list prop -> prop.
   preprocess Ctx SpecT Spec  Cs Progs :-
     remove_trm SpecT Spec,
     build_clauses Ctx Cs,
     env_clauses Ctx Progs.
   
+  type parse_arguments list argument -> cert -> term -> list argument -> prop.
   % default fpc: height
-solve [int N, trm Prog | SpecT] [goal Ctx _Ev Goal _] _OutGoals :-
-        preprocess Ctx SpecT Spec Cs Progs,
-        (Progs => (std.map Spec (x\ y\ copy x y) SpecTypes,
+  parse_arguments [int N, trm Prog | SpecT] (qgen (qheight N)) Prog SpecT.
+  parse_arguments [str "size", int N, trm Prog | SpecT] (qgen (qsize N M_)) Prog SpecT.
+  parse_arguments [str "pair", int N, int S, trm Prog | SpecT] (pair (qgen (qheight N)) (qgen (qsize S S2_ ))) Prog SpecT.
+
+  solve Args [goal Ctx _Ev Goal _] _OutGoals :-
+    parse_arguments Args Cert Prog SpecT,
+    preprocess Ctx SpecT Spec Cs Progs,
+    (Progs => (std.map Spec (x\ y\ copy x y) SpecTypes,
     copy Prog ProgType)),
     (Cs => (std.map SpecTypes (x\y\ copy x y) SpecGoals,
     std.map Spec (x\y\ copy x y) SpecVars,
@@ -77,46 +97,12 @@ solve [int N, trm Prog | SpecT] [goal Ctx _Ev Goal _] _OutGoals :-
 %    coq.say "Spec Vars:" {std.map SpecVars (t\s\ coq.term->string t s)},
 %    coq.say "Prog:" {coq.term->string ProgGoal},
 %    coq.say "Prop:" {coq.term->string PropGoal},
-    std.map SpecGoals (g\t\ check (qgen (qheight N)) (go g t)) SpecVars,
+    std.map SpecGoals (g\t\ check Cert (go g t)) SpecVars,
     % coq.say "Proof Term:" {std.map SpecVars (t\s\ coq.term->string t s)},
     % coq.say "Interp" {coq.term->string ProgGoal},
     interp ProgGoal,
     % coq.say "Run" {coq.term->string PropGoal},
     not (interp PropGoal),
     coq.say "Counterexample:" {coq.term->string ProgGoal}.
-% qsize
-solve [str "size", int N, trm Prog | SpecT] [goal Ctx _Ev Goal _] _OutGoals :-
-      preprocess Ctx SpecT Spec Cs Progs,
-        
-        (Progs => (std.map Spec (x\ y\ copy x y) SpecTypes,
-    copy Prog ProgType)),
-    (Cs => (std.map SpecTypes (x\y\ copy x y) SpecGoals,
-    std.map Spec (x\y\ copy x y) SpecVars,
-    copy ProgType ProgGoal,
-    copy Goal PropGoal)),
- 
-    std.map SpecGoals (g\t\ check (qgen (qsize N M_)) (go g t)) SpecVars,
-    interp ProgGoal,
-    not (interp PropGoal),
-    coq.say "Counterexample:" {coq.term->string ProgGoal}.
-
-    
-% pair
-solve [str "pair", int N, int S, trm Prog | SpecT] [goal Ctx _Ev Goal _] _OutGoals :-
-      preprocess Ctx SpecT Spec Cs Progs,
-        
-        (Progs => (std.map Spec (x\ y\ copy x y) SpecTypes,
-    copy Prog ProgType)),
-    (Cs => (std.map SpecTypes (x\y\ copy x y) SpecGoals,
-    std.map Spec (x\y\ copy x y) SpecVars,
-    copy ProgType ProgGoal,
-    copy Goal PropGoal)),
- 
-    std.map SpecGoals 
-       (g\t\ check (pair (qgen (qheight N)) (qgen (qsize S S2_ )))
-                  (go g t)) SpecVars,
-    interp ProgGoal,
-    not (interp PropGoal),
-    coq.say "Counterexample:" {coq.term->string ProgGoal}.        
 }}.
 Elpi Typecheck.
