1e771326df9b881bd5a6f058ca504dcfe689bb58
Matteo Manighetti <mmanighe@lix.polytechnique.fr>
2021-03-11 17:29:09 +0100

    Some playing around with intexed lists

---
+++ examples/deplist_queries.v
@@ -0,0 +1,11 @@
+From elpi Require Import elpi.
+Require Import dep_pbt.
+Load amDep.
+
+Goal forall (n: nat) (l: ilist nat n), rev l = l.
+intros.
+elpi dep_pbt 5 (True) (l).
+Abort.
+
+Goal exists n, hd (repeat 5 5) = n. 
+intros. Fail elpi dep_pbt 5.
